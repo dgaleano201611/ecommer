5f96e1dc0f0a3d0ccbedcc95393b381e2018fe68
dgaleano201611 <dgaleano201611@gmail.com>
2022-11-14 14:21:25 -0500

    inicializacion del proyecto

---
+++ sportWear/src/main/java/com/ecommerce/sportWear/SportWearApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sportWear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportWearApplication.class, args);
+	}
+
+}
---
+++ sportWear/src/test/java/com/ecommerce/sportWear/SportWearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sportWear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportWearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
